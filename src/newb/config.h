2c72c74bafe7848507c81c3870ec069577f04c9e
pengubah <goboyy2011@gmail.com>
2026-09-13 22:25:42 +0700

    Update config.h

--- src/newb/config.h
+++ src/newb/config.h
@@ -169,6 +169,8 @@
 #define NL_CLOUD3_SPEED 0.005            // 0.0 static ~ 4.0 fast moving
 #define NL_CLOUD3_SHADOW 0.9             // 0.1 subtle ~ 1.0 dark
 #define NL_CLOUD3_SHADOW_OFFSET 0.3      // 0.05 minimal ~ 1.0 large
+#define NL_CLOUD3_DAY_HORIZON_INTENSITY 1.0
+#define NL_CLOUD3_NIGHT_HORIZON_INTENSITY 0.6
 
 /* Aurora settings */
 //#define NL_AURORA 1.2           // [toggle] 0.4 dim ~ 4.0 very bright
